13d2ce7146e48060c5926d1f81031c87e9d2e315
andre2xu <>
2024-10-08 11:41:40 +0100

    Created a router and gave the login page a route.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+// stylesheets
 import './index.css';
 
+// views
+import LoginPage from './views/LoginPage';
 
+
+
+const ROUTER = createBrowserRouter([
+  {
+    path: '/login',
+    element: <LoginPage />
+  }
+]);
 
 const ROOT = ReactDOM.createRoot(document.getElementById('root'));
 
 ROOT.render(
-  <h1>Test</h1>
+  <RouterProvider router={ROUTER} />
 );
