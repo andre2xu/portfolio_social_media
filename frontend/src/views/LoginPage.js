5951583fe3f26ecec6cb5622ebc5361e2b9776ca
andre2xu <>
2024-10-08 12:46:29 +0100

    Created the HTML for the login page and gave the platform logo a width and height.

--- frontend/src/views/LoginPage.js
+++ frontend/src/views/LoginPage.js
@@ -1,7 +1,46 @@
+// static
+import Logo from '../static/Logo.svg';
+
+
+
 function LoginPage() {
     return (
         <div id='login-page'>
-            Login Page
+            <main>
+                <div id='logo-container'>
+                    <img src={Logo} alt='Logo'></img>
+                </div>
+
+                <div id='login-form-container'>
+                    <h1>Join now for a chance to go viral</h1>
+
+                    <form id='login-form' action='/login' method='post'>
+                        <div className='field'>
+                            <label htmlFor='username'>Username</label>
+                            <input type='text' id='username' name='username' />
+                        </div>
+
+                        <div className='field'>
+                            <label htmlFor='password'>Password</label>
+                            <input type='text' id='password' name='password' />
+                        </div>
+
+                        <button type='submit'>Log in</button>
+
+                        <h2>Don't have an account?</h2>
+                        <button>Sign up</button>
+                    </form>
+                </div>
+            </main>
+
+            <footer>
+                <ul>
+                    <li>About</li>
+                    <li>Terms of Service</li>
+                    <li>Privacy Policy</li>
+                    <li>&copy; {new Date().getFullYear()} Virost</li>
+                </ul>
+            </footer>
         </div>
     );
 };
