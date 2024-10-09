9985cd0f0be7c743bdfb694c349e109435a1165f
andre2xu <>
2024-10-09 18:11:09 +0100

    Installed and set up CORS.

--- backend/main.js
+++ backend/main.js
@@ -1,10 +1,15 @@
 const body_parser = require('body-parser');
+const cors = require('cors');
 const express = require('express');
 const backend = express();
 
 // CONFIG
 backend.use(body_parser.json()); // for JSON request data
 backend.use(body_parser.urlencoded({extended: true})); // for URL-encoded request data
+backend.use(cors({
+    origin: ['http://localhost:3000'],
+    optionsSuccessStatus: 200 // for legacy browsers
+}));
 
 // ROUTES
 backend.post('/signup', (req, res) => {
