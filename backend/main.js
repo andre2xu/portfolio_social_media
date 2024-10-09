24ddc2ea677f1dc84cdfbb485ada94a8b086ed20
andre2xu <>
2024-10-09 13:27:38 +0100

    Installed body parser and added it as middleware.

--- backend/main.js
+++ backend/main.js
@@ -1,5 +1,10 @@
+const body_parser = require('body-parser');
 const express = require('express');
 const backend = express();
+
+// config
+backend.use(body_parser.json()); // for JSON request data
+backend.use(body_parser.urlencoded({extended: true})); // for URL-encoded request data
 
 
 
