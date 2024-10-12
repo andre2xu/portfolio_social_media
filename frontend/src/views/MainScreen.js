66645dddfaf166980426755cad17483d5e726073
andre2xu <>
2024-10-12 17:47:44 +0100

    Created and styled a side navbar for non-phone screens.

--- frontend/src/views/MainScreen.js
+++ frontend/src/views/MainScreen.js
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 
 // static
+import Logo from '../static/Logo.svg';
 import NoFill_Icon_Account from '../static/icons/Icon_Account_NoFill.svg';
 import Fill_Icon_Account from '../static/icons/Icon_Account_Fill.svg';
 import NoFill_Icon_Bell from '../static/icons/Icon_Bell_NoFill.svg';
@@ -18,6 +19,22 @@ function MainScreen() {
             <div id='mobile-title-bar'>
                 <h1>My Account</h1>
             </div>
+
+            <nav id='side-navbar'>
+                <img src={Logo} alt='Logo'></img>
+
+                <Link to={'/'} className='no-fill'><img src={NoFill_Icon_Account} alt='Account Icon'></img> <span>My Account</span></Link>
+                <Link to={'/'} className='fill hide'><img src={Fill_Icon_Account} alt='Account Icon'></img> <span>My Account</span></Link>
+
+                <Link to={'/'} className='no-fill'><img src={NoFill_Icon_MagnifyingGlass} alt='Explore Icon'></img> <span>Explore</span></Link>
+                <Link to={'/'} className='fill hide'><img src={Fill_Icon_MagnifyingGlass} alt='Explore Icon'></img> <span>Explore</span></Link>
+
+                <Link to={'/'} className='no-fill'><img src={NoFill_Icon_Bell} alt='Notifications Icon'></img> <span>Notifications</span></Link>
+                <Link to={'/'} className='fill hide'><img src={Fill_Icon_Bell} alt='Notifications Icon'></img> <span>Notifications</span></Link>
+
+                <Link to={'/'} className='no-fill messages-page'><img src={NoFill_Icon_Message} alt='Message Icon'></img> <span>Messages</span></Link>
+                <Link to={'/'} className='fill messages-page hide'><img src={Fill_Icon_Message} alt='Message Icon'></img> <span>Messages</span></Link>
+            </nav>
 
             <main>
                 .
