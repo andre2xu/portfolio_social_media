e79b81341b9e94e3535a949a31dfa5aa06cc5ec3
andre2xu <>
2024-10-16 13:22:41 +0100

    Created and styled the account page's profile section.

--- frontend/src/views/AccountPage.js
+++ frontend/src/views/AccountPage.js
@@ -1,7 +1,105 @@
 function AccountPage() {
     return (
         <div id='account-page'>
-            Account Page
+            <div id='account-page-tabs'>
+                <button>Profile</button>
+                <button>Settings</button>
+            </div>
+
+            <div id='account-page-profile'>
+                <div id='account-page-profile-cover'></div>
+
+                <div id='account-page-profile-info-container'>
+                    <img id='account-page-profile-picture' src='/pfp/Default_Profile_Picture.png' alt='User' />
+
+                    <div id='account-page-profile-info'>
+                        <span>@Username</span>
+
+                        <div>
+                            <div className='profile-counts'>
+                                <span>0</span>
+                                <span>Posts</span>
+                            </div>
+
+                            <div className='profile-counts'>
+                                <span>0</span>
+                                <span>Following</span>
+                            </div>
+
+                            <div className='profile-counts'>
+                                <span>0</span>
+                                <span>Followers</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id='account-page-lists'>
+                <section id='account-page-following-list' className='hide'>
+                    <h1>Following</h1>
+
+                    <div className='users-list'>
+                        <div className='user'>
+                            <img src='/pfp/Default_Profile_Picture.png' alt='User' />
+
+                            <span>@Username</span>
+
+                            <button>Unfollow</button>
+                        </div>
+
+                        <div className='user'>
+                            <img src='/pfp/Default_Profile_Picture.png' alt='User' />
+
+                            <span>@Username</span>
+
+                            <button>Unfollow</button>
+                        </div>
+
+                        <div className='user'>
+                            <img src='/pfp/Default_Profile_Picture.png' alt='User' />
+
+                            <span>@Username</span>
+
+                            <button>Unfollow</button>
+                        </div>
+                    </div>
+
+                    <button className='account-page-lists-show-more'>Show more</button>
+                </section>
+
+                <section id='account-page-followers-list' className='hide'>
+                    <h1>Followers</h1>
+
+                    <div className='users-list'>
+                        <div className='user'>
+                            <img src='/pfp/Default_Profile_Picture.png' alt='User' />
+
+                            <span>@Username</span>
+
+                            <button>Follow</button>
+                        </div>
+
+                        <div className='user'>
+                            <img src='/pfp/Default_Profile_Picture.png' alt='User' />
+
+                            <span>@Username</span>
+
+                            <button>Unfollow</button>
+                        </div>
+
+                        <div className='user'>
+                            <img src='/pfp/Default_Profile_Picture.png' alt='User' />
+
+                            <span>@Username</span>
+
+                            <button>Follow</button>
+                        </div>
+                    </div>
+
+                    <button className='account-page-lists-show-more'>Show more</button>
+                </section>
+            </div>
         </div>
     );
 }
