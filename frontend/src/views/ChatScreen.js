d0382fb8a18a0d6fed7be0229bfbe21b498be1e2
andre2xu <>
2024-10-17 17:39:15 +0100

    Created the UI for chats.

--- frontend/src/views/ChatScreen.js
+++ frontend/src/views/ChatScreen.js
@@ -1,7 +1,28 @@
 function ChatScreen() {
     return (
         <div id='chat-screen' className=''>
-            Chat
+            <div id='chat-screen-chat-info'>
+                <button>&larr;</button>
+
+                <h1>Chat Name</h1>
+                <h2>@Username</h2>
+            </div>
+
+            <div id='chat-screen-conversation'>
+                <div className='message sender'>
+                    Hey I'm sending this message to you
+                </div>
+
+                <div className='message recipient'>
+                    Ok, I received it
+                </div>
+            </div>
+
+            <form id='chat-screen-message-form' action='' method='post'>
+                <input type='text' name='message' placeholder='Message' />
+
+                <button type='submit'>Send</button>
+            </form>
         </div>
     );
 }
