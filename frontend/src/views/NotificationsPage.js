e03d8d38353b0e0a56f2f6b884fe3456b521e831
andre2xu <>
2024-10-18 12:22:48 +0100

    Created the UI for the notifications page settings section.

--- frontend/src/views/NotificationsPage.js
+++ frontend/src/views/NotificationsPage.js
@@ -6,7 +6,7 @@ function Notifications() {
                 <button>Settings</button>
             </div>
 
-            <div id='notifications-page-messages'>
+            <div id='notifications-page-messages' className='hide'>
                 <div className='notification'>
                     <h1>Notification Title</h1>
                     <p>This is the notification body</p>
@@ -20,6 +20,65 @@ function Notifications() {
                 <div className='notification'>
                     <h1>Notification Title</h1>
                     <p>afdjnfiofnoinaoinfwioafawofjnaoifjwfhwfijafowioajhffhowhhfaahwhfuaanohfwohfuoah</p>
+                </div>
+            </div>
+
+            <div id='notifications-page-settings' className=''>
+                <div className='settings-group'>
+                    <h1>Chats</h1>
+
+                    <div className='setting'>
+                        <p>Tell me if someone I follow messaged me.</p>
+
+                        <div className='toggle-button'>
+                            <div className='bar'></div>
+                            <div className='thumb'></div>
+                        </div>
+                    </div>
+
+                    <div className='setting'>
+                        <p>Tell me if someone I don't follow messaged me.</p>
+
+                        <div className='toggle-button'>
+                            <div className='bar'></div>
+                            <div className='thumb'></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='settings-group'>
+                    <h1>Posts</h1>
+
+                    <div className='setting'>
+                        <p>Notify me when someone liked one of my posts.</p>
+
+                        <div className='toggle-button'>
+                            <div className='bar'></div>
+                            <div className='thumb'></div>
+                        </div>
+                    </div>
+
+                    <div className='setting'>
+                        <p>Notify me when there's a new comment in one of my posts.</p>
+
+                        <div className='toggle-button'>
+                            <div className='bar'></div>
+                            <div className='thumb'></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='settings-group'>
+                    <h1>Network</h1>
+
+                    <div className='setting'>
+                        <p>Let me know if someone followed me.</p>
+
+                        <div className='toggle-button'>
+                            <div className='bar'></div>
+                            <div className='thumb'></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
