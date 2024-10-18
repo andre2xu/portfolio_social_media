374b2d9bddee384dd6bce0af7a139f131f3d133f
andre2xu <>
2024-10-18 11:54:05 +0100

    Created the UI for the notifications page messages section.

--- frontend/src/views/NotificationsPage.js
+++ frontend/src/views/NotificationsPage.js
@@ -1,7 +1,27 @@
 function Notifications() {
     return (
         <div id='notifications-page'>
-            
+            <div id='notifications-page-tabs'>
+                <button>Notifications</button>
+                <button>Settings</button>
+            </div>
+
+            <div id='notifications-page-messages'>
+                <div className='notification'>
+                    <h1>Notification Title</h1>
+                    <p>This is the notification body</p>
+                </div>
+
+                <div className='notification'>
+                    <h1>Notification Title</h1>
+                    <p>This is the notification body</p>
+                </div>
+
+                <div className='notification'>
+                    <h1>Notification Title</h1>
+                    <p>afdjnfiofnoinaoinfwioafawofjnaoifjwfhwfijafowioajhffhowhhfaahwhfuaanohfwohfuoah</p>
+                </div>
+            </div>
         </div>
     );
 }
