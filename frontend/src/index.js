0a4bf3893474bad862fad5c49b69428c20867328
andre2xu <>
2024-11-27 13:10:41 +0000

    Created a route for the profile page view.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,6 +14,7 @@ import MainScreen from './views/MainScreen';
 // main screen views
 import ExplorePage from './views/ExplorePage';
 import AccountPage from './views/AccountPage';
+import ProfilePage from './views/ProfilePage';
 import MessagesPage from './views/MessagesPage';
 import NotificationsPage from './views/NotificationsPage';
 
@@ -38,6 +39,10 @@ const ROUTER = createBrowserRouter([
       {
         path: '/account',
         element: <MainScreen component={<AccountPage />} />
+      },
+      {
+        path: '/profile/:username',
+        element: <MainScreen component={<ProfilePage />} />
       },
       {
         path: '/account/messages',
