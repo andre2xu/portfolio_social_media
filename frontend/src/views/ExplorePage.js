a497682e2dcbb53c0cf5a37684e59372bcff4c0a
andre2xu <>
2024-12-07 10:50:41 +0000

    The explore page can now load the latest posts.

--- frontend/src/views/ExplorePage.js
+++ frontend/src/views/ExplorePage.js
@@ -1,17 +1,31 @@
+import React from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
+import shared from '../shared';
 
 // static
-import NoFill_Icon_ThumbsUp from '../static/icons/Icon_ThumbsUp_NoFill.svg';
-import Fill_Icon_ThumbsUp from '../static/icons/Icon_ThumbsUp_Fill.svg';
-import NoFill_Icon_Message from '../static/icons/Icon_Message_NoFill.svg';
 import Fill_Icon_MagnifyingGlass from '../static/icons/Icon_MagnifyingGlass_Fill.svg';
+
+// general views
+import Post from '../components/Post';
 
 
 
 function ExplorePage({isLoggedIn}) {
+    const [posts, loadPosts] = React.useState([]);
+
     function search(event) {
         event.preventDefault();
     };
+
+    React.useEffect(() => {
+        axios.get(shared.resolveBackendRoute('/explore'), {withCredentials: true})
+        .then((response) => {
+            if (response.status === 200 && 'posts' in response.data) {
+                loadPosts(response.data.posts);
+            }
+        });
+    }, []);
 
     return (
         <div id='explore-page' className=''>
@@ -40,108 +54,44 @@ function ExplorePage({isLoggedIn}) {
             }
 
             <div id='explore-page-posts'>
-                <article className='post'>
-                    <div className='user-info'>
-                        <img src='/pfp/Default_Profile_Picture.png' alt='User'></img>
-                        <span>Username</span>
-                    </div>
+                {
+                    posts.map((postData, index) => {
+                        const USER_INFO = {
+                            pfp: '/pfp/Default_Profile_Picture.png',
+                            username: postData.username
+                        };
 
-                    <div className='post-content'>
-                        <p>
-                            This is a regular post.
-                        </p>
+                        if (postData.pfp.length > 0) {
+                            USER_INFO.pfp = shared.resolveBackendRoute(`/static/users/profile/${postData.pfp}`);
+                        }
 
-                        <ul>
-                            <li>#vanilla</li>
-                            <li>#explore</li>
-                        </ul>
-                    </div>
-
-                    <div className='post-info'>
-                        <span className='publish-date'>DD/MM/YYYY</span>
-
-                        <div className='likes'>
-                            <img src={NoFill_Icon_ThumbsUp} alt='Like Icon'></img>
-                            <img className='hide' src={Fill_Icon_ThumbsUp} alt='Like Icon'></img>
-                            <span>100</span>
-                        </div>
-
-                        <div className='comments'>
-                            <img src={NoFill_Icon_Message} alt='Comment Icon'></img>
-                            <span>0</span>
-                        </div>
-                    </div>
-                </article>
-
-                <article className='post'>
-                    <div className='user-info'>
-                        <img src='/pfp/Default_Profile_Picture.png' alt='User'></img>
-                        <span>Username</span>
-                    </div>
-
-                    <div className='post-content'>
-                        <p>
-                            This is a picture.
-                        </p>
-
-                        <img src='/pfp/Default_Profile_Picture.png' alt='Post'></img>
-
-                        <ul>
-                            <li>#pics</li>
-                            <li>#explore</li>
-                        </ul>
-                    </div>
-
-                    <div className='post-info'>
-                        <span className='publish-date'>DD/MM/YYYY</span>
-
-                        <div className='likes'>
-                            <img src={NoFill_Icon_ThumbsUp} alt='Like Icon'></img>
-                            <img className='hide' src={Fill_Icon_ThumbsUp} alt='Like Icon'></img>
-                            <span>100</span>
-                        </div>
-
-                        <div className='comments'>
-                            <img src={NoFill_Icon_Message} alt='Comment Icon'></img>
-                            <span>0</span>
-                        </div>
-                    </div>
-                </article>
-
-                <article className='post'>
-                    <div className='user-info'>
-                        <img src='/pfp/Default_Profile_Picture.png' alt='User'></img>
-                        <span>Username</span>
-                    </div>
-
-                    <div className='post-content'>
-                        <p>
-                            This is a video.
-                        </p>
-
-                        <video src='https://www.pexels.com/download/video/5896379/?fps=23.97599983215332&h=1920&w=1080' loop controls controlsList='nodownload' disablePictureInPicture></video>
-
-                        <ul>
-                            <li>#vids</li>
-                            <li>#explore</li>
-                        </ul>
-                    </div>
-
-                    <div className='post-info'>
-                        <span className='publish-date'>DD/MM/YYYY</span>
-
-                        <div className='likes'>
-                            <img src={NoFill_Icon_ThumbsUp} alt='Like Icon'></img>
-                            <img className='hide' src={Fill_Icon_ThumbsUp} alt='Like Icon'></img>
-                            <span>100</span>
-                        </div>
-
-                        <div className='comments'>
-                            <img src={NoFill_Icon_Message} alt='Comment Icon'></img>
-                            <span>0</span>
-                        </div>
-                    </div>
-                </article>
+                        if (postData.media.length > 0) {
+                            // with media
+                            return (
+                                <Post
+                                    key={index}
+                                    userInfo={USER_INFO}
+                                    postInfo={{pid: postData.pid, body: postData.body, tags: postData.tags, date: postData.date, likes: postData.likes.length, comments: postData.comments}}
+                                    media={{src: shared.resolveBackendRoute(`/static/users/posts/${postData.media[0].src}`), type: postData.media[0].type}}
+                                    isLiked={postData.likedByUser}
+                                    isDeletable={false}
+                                />
+                            );
+                        }
+                        else {
+                            // without media
+                            return (
+                                <Post
+                                    key={index}
+                                    userInfo={USER_INFO}
+                                    postInfo={{pid: postData.pid, body: postData.body, tags: postData.tags, date: postData.date, likes: postData.likes.length, comments: postData.comments}}
+                                    isLiked={postData.likedByUser}
+                                    isDeletable={false}
+                                />
+                            );
+                        }
+                    })
+                }
             </div>
         </div>
     );
