ecdbcbf3a3807f8f9d19c8ad493ad511681d5bc2
andre2xu <>
2024-12-13 13:17:05 +0000

    Added a React effect in the chat screen that requests for and displays chat data. It can now display the chat name and the other user's username.

--- frontend/src/views/ChatScreen.js
+++ frontend/src/views/ChatScreen.js
@@ -1,21 +1,47 @@
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import axios from 'axios';
+import $ from 'jquery';
+import { useNavigate, useParams } from 'react-router-dom';
+import shared from '../shared';
 
 
 
 function ChatScreen() {
+    const URL_PARAMETERS = useParams();
     const redirectTo = useNavigate();
 
     function goBack() {
         redirectTo(-1);
     };
 
+    React.useEffect(() => {
+        axios.get(shared.resolveBackendRoute(`/messages/${URL_PARAMETERS.cid}`), {withCredentials: true})
+        .then((response) => {
+            if (response.status === 200 && 'chatData' in response.data && 'messages' in response.data) {
+                const CHAT_DATA = response.data.chatData;
+                const CHAT_INFO = $('#chat-screen-chat-info');
+
+                // display the chat's name
+                CHAT_INFO.children('h1').text(CHAT_DATA.chatName);
+
+                // display the username of the other user
+                if ('userIsChatOwner' in CHAT_DATA) {
+                    CHAT_INFO.children('h2').text(`@${CHAT_DATA.recipient}`);
+                }
+                else if ('userIsRecipient' in CHAT_DATA) {
+                    CHAT_INFO.children('h2').text(`@${CHAT_DATA.owner}`);
+                }
+            }
+        });
+    }, [URL_PARAMETERS.cid]);
+
     return (
         <div id='chat-screen' className=''>
             <div id='chat-screen-chat-info'>
                 <button onClick={goBack}>&larr;</button>
 
-                <h1>Chat Name</h1>
-                <h2>@Username</h2>
+                <h1> </h1>
+                <h2> </h2>
             </div>
 
             <div id='chat-screen-conversation'>
