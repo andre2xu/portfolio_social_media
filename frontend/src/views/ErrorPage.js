df6f5520b6683836c429d9992a329581c64ea0e0
andre2xu <>
2024-12-17 11:44:41 +0000

    Created the HTML for the error page and implemented code to dynamically show the correct error message for errors 400 and 500.

--- frontend/src/views/ErrorPage.js
+++ frontend/src/views/ErrorPage.js
@@ -1,6 +1,31 @@
+import React from 'react';
+import $ from 'jquery';
+import { useParams } from 'react-router-dom';
+
+
+
 function ErrorPage() {
+    const URL_PARAMETERS = useParams();
+
+    React.useEffect(() => {
+        const ERROR_MESSAGE = $('#error-page-message');
+
+        switch (URL_PARAMETERS.code) {
+            case '500':
+                ERROR_MESSAGE.text("An error occurred in the server. Please come back later.");
+                break;
+            case '404':
+            default:
+                ERROR_MESSAGE.text("The page you were looking for does not exist.");
+        }
+    }, [URL_PARAMETERS]);
+
     return (
-        <h1>Error</h1>
+        <div id='error-page'>
+            <h1>Error {URL_PARAMETERS.code}</h1>
+
+            <p id='error-page-message'></p> 
+        </div>
     );
 };
 
