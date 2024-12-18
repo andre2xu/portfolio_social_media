6afc88d8d69d631953c3e06fe2699c4e4addad4a
andre2xu <>
2024-12-18 14:06:18 +0000

    Added a link in the error page that redirects the user to the home page.

--- frontend/src/views/ErrorPage.js
+++ frontend/src/views/ErrorPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import $ from 'jquery';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 
 
@@ -8,11 +8,11 @@ function ErrorPage() {
     const URL_PARAMETERS = useParams();
 
     React.useEffect(() => {
-        const ERROR_MESSAGE = $('#error-page-message');
+        const ERROR_MESSAGE = $('#error-page-message span');
 
         switch (URL_PARAMETERS.code) {
             case '500':
-                ERROR_MESSAGE.text("An error occurred in the server. Please come back later.");
+                ERROR_MESSAGE.text("An error occurred in the server. Please come back later. ");
                 break;
             case '404':
             default:
@@ -20,7 +20,7 @@ function ErrorPage() {
                     $('#error-page-code').text('Error 404');
                 }
 
-                ERROR_MESSAGE.text("The page you were looking for does not exist.");
+                ERROR_MESSAGE.text("The page you were looking for does not exist. ");
         }
     }, [URL_PARAMETERS]);
 
@@ -28,7 +28,10 @@ function ErrorPage() {
         <div id='error-page'>
             <h1 id='error-page-code'>Error {URL_PARAMETERS.code}</h1>
 
-            <p id='error-page-message'></p> 
+            <p id='error-page-message'>
+                <span></span>
+                <Link to={'/'}>Go to home page</Link>
+            </p>
         </div>
     );
 };
