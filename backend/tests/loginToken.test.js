f14c56093464b17240e3477ef972aba3ecd95c06
andre2xu <>
2024-12-19 16:39:33 +0000

    Created tests for the '/auth' route.

--- backend/tests/loginToken.test.js
+++ backend/tests/loginToken.test.js
@@ -1,6 +1,7 @@
 const shared = require('./shared.test');
 const request = require('supertest');
 const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 
 
 
@@ -25,5 +26,51 @@ describe("Logout", () => {
 
         const LOGOUT_RESPONSE_COOKIES = LOGOUT_RESPONSE.headers['set-cookie'];
         expect(Array.isArray(LOGOUT_RESPONSE_COOKIES) && LOGOUT_RESPONSE_COOKIES.length > 0 && LOGOUT_RESPONSE_COOKIES[0].indexOf('LT=;') !== -1).toBe(true);
+    });
+});
+
+describe("Frontend Authentication", () => {
+    it("No login token is passed. Return 200 and the 'isAuthenticated' flag set to false", async () => {
+        const RESPONSE = await request(shared.BACKEND_URL).post('/auth').set('Cookie', '').send();
+
+        expect(RESPONSE.status).toEqual(200);
+
+        expect(RESPONSE.body).toEqual({isAuthenticated: false});
+    });
+
+    it("Valid login token is passed. Return 200 and the 'isAuthenticated' flag set to true", async () => {
+        const RESPONSE = await request(shared.BACKEND_URL).post('/auth').set('Cookie', test_user_data.loginToken).send();
+
+        expect(RESPONSE.status).toEqual(200);
+
+        expect(RESPONSE.body).toEqual({isAuthenticated: true});
+    });
+
+    it("Invalid login token is passed. Return 200 and the 'isAuthenticated' flag set to false", async () => {
+        // no LT cookie
+        let response = await request(shared.BACKEND_URL).post('/auth').set('Cookie', 'testcookie=1').send();
+
+        expect(response.status).toEqual(200);
+        expect(response.body).toEqual({isAuthenticated: false});
+
+        // invalid JWT
+        response = await request(shared.BACKEND_URL).post('/auth').set('Cookie', 'LT=abc').send();
+
+        expect(response.status).toEqual(200);
+        expect(response.body).toEqual({isAuthenticated: false});
+
+        // expired JWT
+        const EXPIRED_LOGIN_TOKEN = jwt.sign(
+            {
+                uid: test_user_data.uid,
+                exp: Math.floor(Date.now() / 1000) - 1 // old expiration date (i.e. expired 1 second ago)
+            },
+            process.env.LTS
+        );
+
+        response = await request(shared.BACKEND_URL).post('/auth').set('Cookie', `LT=${EXPIRED_LOGIN_TOKEN}`).send();
+
+        expect(response.status).toEqual(200);
+        expect(response.body).toEqual({isAuthenticated: false});
     });
 });
