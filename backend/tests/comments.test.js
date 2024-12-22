598dc3704da24a04dfc75044b471a75020fe68db
andre2xu <>
2024-12-22 12:07:49 +0000

    Created invalid request tests for the '/comments/:pid' POST route.

---
+++ backend/tests/comments.test.js
@@ -0,0 +1,68 @@
+const shared = require('./shared.test');
+const request = require('supertest');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
+
+
+// NOTE: Start the server first before running the tests
+
+let test_user_data = {};
+let test_post_data = {};
+let mongo_client = undefined;
+
+beforeAll(async () => {
+    test_user_data = await shared.createTestUser(crypto.randomBytes(5).toString('hex'), '!aB0aaaaaaaaaaaa');
+
+    mongo_client = await shared.openDatabaseConnection();
+
+    // create a test post
+    const POST_RESPONSE = await request(shared.BACKEND_URL).post('/post').set('Cookie', test_user_data.loginToken)
+        .field('postBody', "Add a comment to me plz")
+        .field('postTags', 'tag');
+
+    shared.expectEmptyJSONResponse(POST_RESPONSE);
+
+    const POSTS_COLLECTION = mongo_client.db('socialmedia').collection('Posts');
+    const POST = await POSTS_COLLECTION.findOne({uid: test_user_data.uid}, {projection: {_id: 0}});
+
+    expect(POST).not.toEqual(null);
+
+    test_post_data = POST;
+});
+
+afterAll(async () => {
+    shared.deleteTestUsers([test_user_data.uid]);
+
+    mongo_client.close();
+
+    // delete test post
+    const POSTS_COLLECTION = mongo_client.db('socialmedia').collection('Posts');
+
+    await POSTS_COLLECTION.deleteOne({pid: test_post_data.pid});
+});
+
+describe("Creating Comments", () => {
+    it("No login token passed. Return 200 and an empty JSON object", async () => {
+        const RESPONSE = await request(shared.BACKEND_URL).post(`/comments/${test_post_data.pid}`).send();
+
+        shared.expectEmptyJSONResponse(RESPONSE);
+    });
+
+    it("Invalid request bodies. Return 200 and an empty JSON object", async () => {
+        // non-existent post
+        let response = await request(shared.BACKEND_URL).post('/comments/notapost').set('Cookie', test_user_data.loginToken).send();
+
+        shared.expectEmptyJSONResponse(response);
+
+        // non-existent user
+        const LOGIN_TOKEN = jwt.sign(
+            {uid: crypto.randomBytes(5).toString('hex')},
+            process.env.LTS
+        );
+
+        response = await request(shared.BACKEND_URL).post(`/comments/${test_post_data.pid}`).set('Cookie', `LT=${LOGIN_TOKEN}`).send();
+
+        shared.expectEmptyJSONResponse(response);
+    });
+});
